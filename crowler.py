f2bf20e867458e48bdd0615dcaff32cf9e8c90e6
Rasul <glaha.rasik@gmail.com>
2023-02-26 01:14:26 +0300

    prepare

--- crowler.py
+++ crowler.py
@@ -68,7 +68,7 @@ def scrap_shedule_matches( ):
 
 def get_lifecycle():
     keeper = Storage()
-    keeper.transit_to_results()
+    keeper.tranjusit_to_results()
     keeper.get_today()
         
 
